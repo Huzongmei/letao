3c8c5f7207789eab837a119356e357dda3db82cd
Huzongmei <1010725069@qq.com>
2018-03-02 18:23:00 +0800

    登录页面第二次提交

--- back/js/login.js
+++ back/js/login.js
@@ -7,9 +7,8 @@
     //2. 用户名长度为3-6位
     //3. 用户密码不能为空
     //4. 用户密码长度为6-12位
-
-
     $('form').bootstrapValidator({
+        //校验字段
         fields:{
             username:{
                 validators:{
@@ -35,6 +34,34 @@
                     }
                 }
             }
+        },
+        //添加校验小图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
         }
+    });
+
+
+    //点击重置按钮 表单校验信息隐藏
+    $('[type="reset"]').on('click',function(){
+        $('form').data('bootstrapValidator').resetForm();
+    });
+
+    //给表单注册一个校验成功的事件，当校验成功时，阻止表单的默认提交
+    $('form').on('success.form.bv',function(e){
+        e.preventDefault();
+
+        //ajax请求后台验证用户登录
+        $.ajax({
+            type:'POST',
+            url:'/employee/employeeLogin',
+            data: $('form').serialize(),
+            success:function(info){
+                console.log(info);
+            }
+        })
     })
+
 })();
