2b4e8806a4676b1179083f43fc7de2a2fd656282
Huzongmei <1010725069@qq.com>
2018-03-04 19:15:40 +0800

    进度条设置完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -3,12 +3,18 @@
  */
 $(function(){
     //设置页面加载进度条
+    //禁用进度环
+    NProgress.configure({
+        showSpinner: false
+    });
     $(document).ajaxStart(function(){
         //ajax发送请求开始时，开启进度条
         NProgress.start();
     });
     $(document).ajaxStop(function(){
         //ajax发送请求结束时，结束进度条
-        NProgress.done();
+        setTimeout(function(){
+            NProgress.done();
+        },500)
     })
 });
