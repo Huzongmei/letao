54bf6a1aa0ef04d44e4d442a251cb52eece38478
Huzongmei <1010725069@qq.com>
2018-03-04 20:41:46 +0800

    验证登录完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -8,6 +8,57 @@ $(function(){
     });
     $(document).ajaxStop(function(){
         NProgress.done();
-    })
+    });
+
+
+
+    //二级菜单的显示和隐藏
+    $('.category').prev().on('click',function(){
+        $(this).next().slideToggle();
+    });
+
+
+    //侧边栏显示和隐藏
+    $('.icon_menu').on('click',function(){
+        $('.lt_aside').toggleClass('now');
+        $('.lt_main').toggleClass('now');
+    });
+
+
+    //退出功能
+    //点击退出 显示模态框
+    $('.icon_loginout').on('click',function(){
+        $('#modal').modal('show');
+    });
+    //点击退出，ajax请求后台，实现退出
+    $('.btn_loginout').on('click',function(){
+        $.ajax({
+            type:'GET',
+            url:'/employee/employeeLogout',
+            dataType:'json',
+            success:function(info){
+                //console.log(info);
+                if(info.success){
+                    location.href='login.html';
+                }
+            }
+        })
+    });
+
+
+    //如果不是登录页，发送ajax请求，查询管理员是否登录
+    if(location.href.indexOf('login.html')){
+        $.ajax({
+            type:'GET',
+            url:'/employee/checkRootLogin',
+            dataType:'json',
+            success:function(info){
+                //console.log(info);
+                if(!info.success){
+                    location.href='login.html'
+                }
+            }
+        })
+    }
 });
 
