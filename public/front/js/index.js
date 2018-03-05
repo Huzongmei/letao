dc217ef6d628988f06aa0b0d8947d99d4449256c
Huzongmei <1010725069@qq.com>
2018-03-05 20:33:30 +0800

    index静态页面完成

---
+++ public/front/js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Admin on 2018/3/5.
+ */
+;(function(){
+
+    // 图片轮播初始化
+    var gallery = mui('.mui-slider');
+    gallery.slider({
+        interval:3000//自动轮播周期，若为0则不自动播放，默认为0；
+    });
+
+
+})();
+
