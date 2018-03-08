189d1875567a4b1c38ea6889cd7c530918b49438
Huzongmei <1010725069@qq.com>
2018-03-08 13:56:07 +0800

    商品列表完成

--- public/front/js/searchList.js
+++ public/front/js/searchList.js
@@ -37,5 +37,46 @@ $(function(){
         //把value值拼接到地址栏后 重新渲染
         obj.proName=value;
         render();
-    })
+    });
+
+    //给search_nav下的a注册点击事件
+    $('.search_nav a[data-type]').on('click',function(){
+        //修改样式 字体颜色 小图标 排他
+        if($(this).hasClass('now')){
+            $(this).find('span').toggleClass('fa-angle-down').toggleClass('fa-angle-up');
+
+
+        }else{
+            $(this).addClass('now').siblings().removeClass('now');
+            $('.search_nav a span').removeClass('fa-angle-up').addClass('fa-angle-down');
+        }
+
+        //根据search_nav下的a样式请求后台数据
+        if($(this).data('type')=='price'){
+            if(obj.num){
+                delete obj.num;
+            }
+            if($(this).find('span').hasClass('fa-angle-down')){
+                obj.price=2;
+            }else{
+                obj.price=1;
+
+            }
+        }
+        if($(this).data('type')=='num'){
+            if(obj.price){
+                delete obj.price;
+            }
+            if($(this).find('span').hasClass('fa-angle-down')){
+                obj.num=2;
+            }else{
+                obj.num=1;
+            }
+        }
+        render();
+    });
+
+
+
+
 });
